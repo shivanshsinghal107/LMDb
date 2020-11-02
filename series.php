ec86a49396a5ca7db63e184d497d2f25ed129744
shivanshsinghal107 <shivanshsinghal107@gmail.com>
2020-11-02 17:03:53 +0530

    Added movie page & series page for the web app

---
+++ series.php
@@ -0,0 +1,53 @@
+<?php
+
+$id = $_GET['id'];
+$image = 'image'.$id.'.jpg';
+$path = "series/$image";
+
+$host = 'localhost';
+$user = 'root';
+$password = '';
+$db_name = 'lmdb';
+
+// connect to the database
+$connection = mysqli_connect($host, $user, $password, $db_name);
+$query = "SELECT * FROM `series` WHERE id = '$id'";
+$res = mysqli_query($connection, $query);
+
+$row = mysqli_fetch_assoc($res);
+$name = $row['name'];
+$genre = $row['genre'];
+$rating = $row['rating'];
+$year = $row['year'];
+$duration = $row['duration'];
+$seasons = $row['seasons'];
+$episodes = $row['episodes'];
+$plot = $row['plot'];
+$cast = $row['cast'];
+
+$actors = explode(", ", $cast);
+if($id > 10 && $id <= 20)
+  $id -= 10;
+else if($id > 20)
+  $id -= 20;
+
+echo "<img src='$path' style='width:400px;height:600px; float:left; margin-right: 25px;' alt='cover photo'>";
+echo "<h1>$name (Rank: $id)</h1>";
+echo "<h2>Genres: $genre<br>";
+echo "Rating: $rating<br>";
+echo "Release: $year<br>";
+echo "Seasons: $seasons<br>";
+echo "Episodes: $episodes<br>";
+echo "Avg. Duration: $duration minutes<br></h2>";
+echo "<h2>Cast:</h2><h3><ul>";
+for($i = 0; $i < count($actors); $i++){
+  $actor = explode(" /", $actors[$i]);
+  echo "<li>".$actor[0];
+  if(count($actor) > 1)
+    echo ")";
+  echo "</li>";
+}
+echo "</ul></h3>";
+echo "<h2>Plot:</h2><h3>$plot</h3>";
+
+?>
