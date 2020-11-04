0dc09454f0cd56d55eca420038e845263867c82d
shivanshsinghal107 <shivanshsinghal107@gmail.com>
2020-11-04 13:42:43 +0530

    Added option to give rating and review for logged in user

--- movies.php
+++ movies.php
@@ -4,47 +4,61 @@ $id = $_GET['id'];
 $image = 'image'.$id.'.jpg';
 $path = "movies/$image";
 
-$host = 'localhost';
-$user = 'root';
-$password = '';
-$db_name = 'lmdb';
+$flag = 0;
 
 // connect to the database
-$connection = mysqli_connect($host, $user, $password, $db_name);
-$query = "SELECT * FROM `movies` WHERE id = '$id'";
+$connection = mysqli_connect('localhost', 'root', '', 'lmdb');
+$query = "SELECT * FROM movies WHERE id = '$id'";
 $res = mysqli_query($connection, $query);
 
-$row = mysqli_fetch_assoc($res);
-$name = $row['name'];
-$genre = $row['genre'];
-$rating = $row['rating'];
-$year = $row['year'];
-$duration = $row['duration'];
-$plot = $row['plot'];
-$cast = $row['cast'];
-$budget = $row['budget'];
-//$gross = $row['worldwide_gross'];
+if(mysqli_num_rows($res) > 0){
+  $row = mysqli_fetch_assoc($res);
+  $name = $row['name'];
+  $genre = $row['genre'];
+  $rating = $row['rating'];
+  $year = $row['year'];
+  $duration = $row['duration'];
+  $plot = $row['plot'];
+  $cast = $row['cast'];
+  $budget = $row['budget'];
+  //$gross = $row['worldwide_gross'];
 
-$actors = explode(", ", $cast);
-if($id > 10)
-  $id -= 10;
+  $actors = explode(", ", $cast);
+  if($id > 10){
+    $id -= 10;
+    $flag = 1;
+  }
 
-echo "<img src='$path' style='width:400px;height:100%; float:left; margin-right: 25px;' alt='cover photo'>";
-echo "<h1>$name (Rank: $id)</h1>";
-echo "<h2>Genres: $genre<br>";
-echo "Rating: $rating<br>";
-echo "Release: $year<br>";
-echo "Duration: $duration minutes<br></h2>";
-echo "<h2>Cast:</h2><h3><ul>";
-for($i = 0; $i < count($actors); $i++){
-  $actor = explode("/", $actors[$i]);
-  echo "<li>".$actor[0];
-  if(count($actor) > 1)
-    echo ")";
-  echo "</li>";
+  echo "<img src='$path' style='width:400px;height:100%; float:left; margin-right: 25px;' alt='cover photo'>";
+  echo "<h1>$name (Rank: $id)</h1>";
+  echo "<h2>Genres: $genre<br>";
+  echo "Rating: $rating<br>";
+  echo "Release: $year<br>";
+  echo "Duration: $duration minutes<br></h2>";
+  echo "<h2>Cast:</h2><h3><ul>";
+  for($i = 0; $i < count($actors); $i++){
+    $actor = explode("/", $actors[$i]);
+    echo "<li>".$actor[0];
+    if(count($actor) > 1)
+      echo ")";
+    echo "</li>";
+  }
+  echo "</ul></h3>";
+  echo "<h2>Plot:</h2><h3>$plot</h3>";
+  echo "<h2>Budget: $budget</h2>";
+
+  if($flag)
+    $id += 10;
+
+  if(isset($_COOKIE['username'])){
+    echo "<form action='mreview.php' method='POST'>";
+    echo "Rating: <input type='number' name='rating' min='1' max='10'><br>";
+    echo "Review: <input type='text' name='review'><br>";
+    echo "<input type='hidden' name='id' value='$id'>";
+    echo "<input type='submit'></form>";
+  }
 }
-echo "</ul></h3>";
-echo "<h2>Plot:</h2><h3>$plot</h3>";
-echo "<h2>Budget: $budget</h2>";
+else
+  echo "<script>alert('Select some movie'); window.location = 'http://localhost/lmdb/lmdb.php';</script>";
 
 ?>
--- series.php
+++ series.php
@@ -4,50 +4,68 @@ $id = $_GET['id'];
 $image = 'image'.$id.'.jpg';
 $path = "series/$image";
 
-$host = 'localhost';
-$user = 'root';
-$password = '';
-$db_name = 'lmdb';
+$flag = 0;
 
 // connect to the database
-$connection = mysqli_connect($host, $user, $password, $db_name);
-$query = "SELECT * FROM `series` WHERE id = '$id'";
+$connection = mysqli_connect('localhost', 'root', '', 'lmdb');
+$query = "SELECT * FROM series WHERE id = '$id'";
 $res = mysqli_query($connection, $query);
 
-$row = mysqli_fetch_assoc($res);
-$name = $row['name'];
-$genre = $row['genre'];
-$rating = $row['rating'];
-$year = $row['year'];
-$duration = $row['duration'];
-$seasons = $row['seasons'];
-$episodes = $row['episodes'];
-$plot = $row['plot'];
-$cast = $row['cast'];
+if(mysqli_num_rows($res) > 0){
+  $row = mysqli_fetch_assoc($res);
+  $name = $row['name'];
+  $genre = $row['genre'];
+  $rating = $row['rating'];
+  $year = $row['year'];
+  $duration = $row['duration'];
+  $seasons = $row['seasons'];
+  $episodes = $row['episodes'];
+  $plot = $row['plot'];
+  $cast = $row['cast'];
 
-$actors = explode(", ", $cast);
-if($id > 10 && $id <= 20)
-  $id -= 10;
-else if($id > 20)
-  $id -= 20;
+  $actors = explode(", ", $cast);
+  if($id > 10 && $id <= 20){
+    $id -= 10;
+    $flag = 1;
+  }
+  else if($id > 20){
+    $id -= 20;
+    $flag = 2;
+  }
 
-echo "<img src='$path' style='width:400px;height:600px; float:left; margin-right: 25px;' alt='cover photo'>";
-echo "<h1>$name (Rank: $id)</h1>";
-echo "<h2>Genres: $genre<br>";
-echo "Rating: $rating<br>";
-echo "Release: $year<br>";
-echo "Seasons: $seasons<br>";
-echo "Episodes: $episodes<br>";
-echo "Avg. Duration: $duration minutes<br></h2>";
-echo "<h2>Cast:</h2><h3><ul>";
-for($i = 0; $i < count($actors); $i++){
-  $actor = explode(" /", $actors[$i]);
-  echo "<li>".$actor[0];
-  if(count($actor) > 1)
-    echo ")";
-  echo "</li>";
+  echo "<img src='$path' style='width:400px;height:600px; float:left; margin-right: 25px;' alt='cover photo'>";
+  echo "<h1>$name (Rank: $id)</h1>";
+  echo "<h2>Genres: $genre<br>";
+  echo "Rating: $rating<br>";
+  echo "Release: $year<br>";
+  echo "Seasons: $seasons<br>";
+  echo "Episodes: $episodes<br>";
+  echo "Avg. Duration: $duration minutes<br></h2>";
+  echo "<h2>Cast:</h2><h3><ul>";
+  for($i = 0; $i < count($actors); $i++){
+    $actor = explode(" /", $actors[$i]);
+    echo "<li>".$actor[0];
+    if(count($actor) > 1)
+      echo ")";
+    echo "</li>";
+  }
+  echo "</ul></h3>";
+  echo "<h2>Plot:</h2><h3>$plot</h3>";
+
+  if($flag == 1)
+    $id += 10;
+  else if($flag == 2)
+    $id += 20;
+
+  if(isset($_COOKIE['username'])){
+    echo "<form action='sreview.php' method='POST'>";
+    echo "Rating: <input type='number' name='rating' min='1' max='10'><br>";
+    echo "Review: <input type='text' name='review'><br>";
+    echo "<input type='hidden' name='id' value='$id'>";
+    echo "<input type='submit'></form>";
+  }
 }
-echo "</ul></h3>";
-echo "<h2>Plot:</h2><h3>$plot</h3>";
+else
+  echo "<script>alert('Select some series'); window.location = 'http://localhost/lmdb/lmdb.php';</script>";
 
 ?>
