ed3904bc19eb8dcb7523241a01437d25558b89f6
shivanshsinghal107 <shivanshsinghal107@gmail.com>
2020-11-04 13:43:26 +0530

    Added files to store rating & review of each user into the database

---
+++ mreview.php
@@ -0,0 +1,19 @@
+<?php
+
+$username = $_COOKIE['username'];
+
+$rating = $_POST['rating'];
+$review = $_POST['review'];
+$id = $_POST['id'];
+
+// connect to the database
+$connection = mysqli_connect('localhost', 'root', '', 'lmdb');
+$query = "INSERT INTO mreviews (mid, username, rating, review) VALUES ('$id', '$username', '$rating', '$review')";
+$res = mysqli_query($connection, $query);
+
+if($res)
+  echo "<script>alert('Rating & Review submitted'); window.location = window.history.back();</script>";
+else
+  echo "<script>alert('Error submitting rating & review'); window.location = window.history.back();</script>";
+
+?>
