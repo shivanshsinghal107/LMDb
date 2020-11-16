46826a2e93f668cc3860050b152fd71e948f3e10
shivanshsinghal107 <shivanshsinghal107@gmail.com>
2020-11-16 15:31:19 +0530

    Linked the css stylesheet

--- lmdb.php
+++ lmdb.php
@@ -6,6 +6,7 @@
 
   <!-- Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-CuOF+2SnTUfTwSZjCXf01h7uYhfOBuxIhGKPbfEJ3+FqH/s6cIFN9bGr1HmAg4fQ" crossorigin="anonymous">
+  <link href="static/index.css" rel="stylesheet">
 
   <style>
 
