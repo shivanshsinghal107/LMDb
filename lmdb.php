6402a53d0949f08e927247eb35c5a6f5dfde83c5
shivanshsinghal107 <shivanshsinghal107@gmail.com>
2020-11-24 22:41:39 +0530

    Added option to search any movie or series

--- lmdb.php
+++ lmdb.php
@@ -73,8 +73,8 @@
             </div>
           </li>
         </ul>
-        <form class="d-flex">
-          <input class="form-control mr-2" type="search" placeholder="Search" aria-label="Search">
+        <form class="d-flex" action="search.php">
+          <input class="form-control mr-2" name="search" type="search" placeholder="Search" aria-label="Search">
           <button class="btn btn-light" type="submit">
             <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-search" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
               <path fill-rule="evenodd" d="M10.442 10.442a1 1 0 0 1 1.415 0l3.85 3.85a1 1 0 0 1-1.414 1.415l-3.85-3.85a1 1 0 0 1 0-1.415z"/>
