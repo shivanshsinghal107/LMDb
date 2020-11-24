e56938fcb211f7be73af20cbc21e2a295e365cb3
shivanshsinghal107 <shivanshsinghal107@gmail.com>
2020-11-24 22:41:12 +0530

    Modified code according to insert or update rating, review by a user

--- sreview.php
+++ sreview.php
@@ -8,12 +8,26 @@ $id = $_POST['id'];
 
 // connect to the database
 $connection = mysqli_connect('localhost', 'root', '', 'lmdb');
-$query = "INSERT INTO sreviews (sid, username, rating, review) VALUES ('$id', '$username', '$rating', '$review')";
-$res = mysqli_query($connection, $query);
 
-if($res)
-  echo "<script>alert('Rating & Review submitted'); window.location = window.history.back();</script>";
-else
-  echo "<script>alert('Error submitting rating & review'); window.location = window.history.back();</script>";
+$query = "SELECT * FROM sreviews WHERE sid = '$id' AND username = '$username'";
+$res = mysqli_query($connection, $query);
+if(mysqli_num_rows($res) > 0){
+  $query = "UPDATE sreviews SET rating = '$rating', review = '$review' WHERE sid = '$id' AND username = '$username'";
+  $res = mysqli_query($connection, $query);
+
+  if($res)
+    echo "<script>alert('Rating & Review updated'); window.location = window.history.back();</script>";
+  else
+    echo "<script>alert('Error updating rating & review'); window.location = window.history.back();</script>";
+}
+else{
+  $query = "INSERT INTO sreviews (sid, username, rating, review) VALUES ('$id', '$username', '$rating', '$review')";
+  $res = mysqli_query($connection, $query);
+
+  if($res)
+    echo "<script>alert('Rating & Review submitted'); window.location = window.history.back();</script>";
+  else
+    echo "<script>alert('Error submitting rating & review'); window.location = window.history.back();</script>";
+}
 
 ?>
